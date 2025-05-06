d554b6d4d5003c988686aa9bed3948e7a96c4e9e
Mehul Uttam <mehulsavio@gmail.com>
2025-05-07 01:03:32 +0530

    Arrays

---
+++ Arrays/Array.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        //syntax
+        // datatype[] variable_name = new datatype[size];
+
+        //store 5 roll nos.
+        int[] rnos = new int[5];
+
+        //or
+        int[] rnos2 = {23, 44, 51, 12, 4}; //It will only contain Integer type only
+
+        //or
+        int[] rnos3; //declaratiopn of array
+        rnos3 = new int[5]; //here object is getting created in the memory in the heap
+
+
+    }
+}
